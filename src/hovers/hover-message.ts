a54b4842b9fc86f77e002249e6714104f9c4bfec
stoyan.yordanov <stoyan.yordanov@gmail.com>
2019-10-21 21:59:20 +0300

    Update hover messges

--- src/hovers/hover-message.ts
+++ src/hovers/hover-message.ts
@@ -1,19 +1,50 @@
 import { MarkdownString } from 'vscode';
-import { Extension, RunCommandArgs, Commands } from '../types';
+import { Extension, RunCommandArgs, Commands, RunCommandMarkdown } from '../types';
 
 export default class HoverMessage {
 	constructor(private readonly extension: Extension) {}
 
-	public getMessage(args: RunCommandArgs): MarkdownString {
+	private getRunCommandMarkdown(command: RunCommandMarkdown, args: RunCommandArgs): MarkdownString {
 		const runCommand = this.extension.commands[Commands.RUN_TEST];
+		const markdown = new MarkdownString(runCommand.getMarkdown(command, args));
+		markdown.isTrusted = true;
+		return markdown;
+	}
 
-		const runTestCommandMessage = runCommand.getMarkdown({ title: 'Run Test' }, args);
-		const runFileCommandMessage = runCommand.getMarkdown({ title: 'Run File' }, { ...args, test: undefined });
+	private getRunTestMarkdown(args: RunCommandArgs): MarkdownString {
+		return this.getRunCommandMarkdown({ title: 'Run Test', description: 'Run current test' }, args);
+	}
 
-		const runTestCommandForceConfigMessage = runCommand.getMarkdown({ title: 'Run Test *' }, { ...args, forceConfig: true });
-		const runFileCommandForceConfigMessage = runCommand.getMarkdown({ title: 'Run File *' }, { ...args, test: undefined, forceConfig: true });
+	private getRunFileMarkdown(args: RunCommandArgs): MarkdownString {
+		return this.getRunCommandMarkdown({ title: 'Run File', description: 'Run current file' }, { ...args, test: undefined });
+	}
 
-		const message = `${runTestCommandMessage} | ${runFileCommandMessage} | ${runTestCommandForceConfigMessage} | ${runFileCommandForceConfigMessage}`;
+	private getRunTestConfigMarkdown(args: RunCommandArgs): MarkdownString {
+		return this.getRunCommandMarkdown(
+			{ title: 'Run Test *', description: 'Run current test with last known config' },
+			{ ...args, forceConfig: true }
+		);
+	}
+
+	private getRunFileConfigMarkdown(args: RunCommandArgs): MarkdownString {
+		return this.getRunCommandMarkdown(
+			{ title: 'Run File *', description: 'Run current file with last known config' },
+			{ ...args, test: undefined, forceConfig: true }
+		);
+	}
+
+	public getMessage(args: RunCommandArgs): MarkdownString {
+		const runTestCommandMarkdown = this.getRunTestMarkdown(args);
+		const runFileCommandMarkdown = this.getRunFileMarkdown(args);
+		const runTestCommandConfigMarkdown = this.getRunTestConfigMarkdown(args);
+		const runFileCommandConfigMarkdown = this.getRunFileConfigMarkdown(args);
+
+		const message = [
+			runTestCommandMarkdown.value,
+			runFileCommandMarkdown.value,
+			runTestCommandConfigMarkdown.value,
+			runFileCommandConfigMarkdown.value
+		].join(' ● ');
 
 		const markdown = new MarkdownString(message);
 		markdown.isTrusted = true;
