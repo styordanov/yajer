b872371fef9b3b8bf001a5785a72e3f466768cef
stoyan.yordanov <stoyan.yordanov@gmail.com>
2019-10-24 10:48:31 +0300

    Update decorator; use text instead of icon

--- src/decorators/decorator.ts
+++ src/decorators/decorator.ts
@@ -1,32 +1,25 @@
+import * as path from 'path';
 import { DecorationOptions, TextEditorDecorationType, Uri, window } from 'vscode';
 import { Extension } from '../types';
 
 export default class Decorator {
-  private timeout: NodeJS.Timer;
-  private decorations: DecorationOptions[] = [];
-  public static decorationType: TextEditorDecorationType = window.createTextEditorDecorationType({
-    after: { margin: '0 0 0 1rem' }
-  });
+	private timeout: NodeJS.Timer;
+	public static decorationType: TextEditorDecorationType = window.createTextEditorDecorationType({
+		after: { margin: '0 0 0 .5rem', contentText: '►', color: '#05ce05' }
+	});
 
-  constructor(private readonly extension: Extension) {}
+	constructor(private readonly extension: Extension) {}
 
-  private getDecorations() {
-    const contentIconPath = Uri.file(this.extension.context.asAbsolutePath('media/lab.svg').replace(/\\/g, '/'));
+	private getDecorations(): DecorationOptions[] {
+		return this.extension.document.getTests().map(({ range }) => ({ range }));
+	}
 
-    const tests = this.extension.document.getTests();
-    this.decorations = tests.map(({ range }) => ({
-      range,
-      renderOptions: { after: { contentIconPath } }
-    }));
-    return this.decorations;
-  }
+	private setDecorations() {
+		this.extension.editor && this.extension.editor.setDecorations(Decorator.decorationType, this.getDecorations());
+	}
 
-  private setDecorations() {
-    this.extension.editor && this.extension.editor.setDecorations(Decorator.decorationType, this.getDecorations());
-  }
-
-  public update() {
-    this.timeout && clearTimeout(this.timeout);
-    this.timeout = setTimeout(this.setDecorations.bind(this), 500);
-  }
+	public update() {
+		this.timeout && clearTimeout(this.timeout);
+		this.timeout = setTimeout(this.setDecorations.bind(this), 500);
+	}
 }
