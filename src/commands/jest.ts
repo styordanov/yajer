442a9181b8ba5478e5f29165b188449a88e0e85d
stoyan.yordanov <stoyan.yordanov@gmail.com>
2019-10-24 21:14:49 +0300

    Add jest executable

---
+++ src/commands/jest.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import { workspace } from 'vscode';
+import { URL } from 'url';
+
+export default class Jest {
+	private static WIN_PATH = 'node_modules/jest/bin/jest.js';
+	private static UNIX_PATH = 'node_modules/.bin/jest';
+
+	private static getWorkspaceFolderPath(): string {
+		const [workspaceFolder] = workspace.workspaceFolders;
+		return workspaceFolder ? workspaceFolder.uri.path : './';
+	}
+
+	static getConfigs(): string[] {
+		const CONFIG_REGEX = /jest.+--config=([a-z0-9\/.]+)/i;
+
+		const packageJSON = path.join(Jest.getWorkspaceFolderPath(), 'package.json');
+		const contents = readFileSync(new URL(`file://${packageJSON}`), 'utf8');
+
+		const configs = contents.split('\n').reduce((configs, textLine) => {
+			const matches = CONFIG_REGEX.exec(textLine);
+			matches && configs.push(matches[1]);
+			return configs;
+		}, []);
+
+		return [...new Set(configs)];
+	}
+
+	static getExecutable(): string {
+		const jestPath = process.platform.includes('win32') ? Jest.WIN_PATH : Jest.UNIX_PATH;
+		return path.join(Jest.getWorkspaceFolderPath(), jestPath);
+	}
+}
--- src/quickpicks/config-quickpick.ts
+++ src/quickpicks/config-quickpick.ts
@@ -1,30 +1,8 @@
-import { URL } from 'url';
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import { workspace, window } from 'vscode';
+import { window } from 'vscode';
+import Jest from '../commands/jest';
 
 export default class ConfigQuickPick {
-	private getWorkspaceFolderPath(): string {
-		const [workspaceFolder] = workspace.workspaceFolders;
-		return workspaceFolder ? workspaceFolder.uri.path : './';
-	}
-
-	private getConfigs(): string[] {
-		const CONFIG_REGEX = /jest.+--config=([a-z0-9\/.]+)/i;
-
-		const packageJSON = join(this.getWorkspaceFolderPath(), 'package.json');
-		const contents = readFileSync(new URL(`file://${packageJSON}`), 'utf8');
-
-		const configs = contents.split('\n').reduce((configs, textLine) => {
-			const matches = CONFIG_REGEX.exec(textLine);
-			matches && configs.push(matches[1]);
-			return configs;
-		}, []);
-
-		return [...new Set(configs)];
-	}
-
 	show() {
-		return window.showQuickPick(this.getConfigs());
+		return window.showQuickPick(Jest.getConfigs());
 	}
 }
